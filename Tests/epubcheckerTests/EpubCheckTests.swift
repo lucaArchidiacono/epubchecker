511d45a02c569e144243e225e59f957ecc4317a5
lucaArchidiacono <luca@swift-mail.com>
2024-01-08 15:57:59 +0100

    Add tests

--- Tests/epubcheckerTests/EpubCheckTests.swift
+++ Tests/epubcheckerTests/EpubCheckTests.swift
@@ -3,13 +3,108 @@ import XCTest
 
 final class epubcheckerTests: XCTestCase {
 
-    func testExample() async throws {
-		let testBook = try getTestBooks()[2]
-		print(testBook)
+    func testValidationWithEncodingOfEpubInspectorUsingAsyncAwait() async {
+		do {
+			for testBook in try getTestBooks() {
+				let epubCheck = EpubCheck(testBook)
+				_ = try await epubCheck.validate()
+			}
+		} catch {
+			XCTFail("\(error)")
+		}
+	}
 
-		let epubCheck = EpubCheck(testBook)
-		let result = try await epubCheck.validate(output: .xml)
-		print(result)
+	func testValidationWithJSONStringOutputUsingAsyncAwait() async {
+		do {
+			for testBook in try getTestBooks() {
+				let epubCheck = EpubCheck(testBook)
+				let output = try await epubCheck.validate(output: .json)
+				XCTAssertTrue(!output.isEmpty)
+			}
+		} catch {
+			XCTFail("\(error)")
+		}
+	}
+
+    func testValidationWithEncodingOfEpubInspectorUsingAsyncAwait() {
+		do {
+			for testBook in try getTestBooks() {
+				let epubCheck = EpubCheck(testBook)
+				epubCheck.validate { result in
+					switch result {
+					case .success: break
+					case .failure(let error):
+						XCTFail("\(error)")
+					}
+				}
+			}
+		} catch {
+			XCTFail("\(error)")
+		}
+	}
+
+	func testValidationWithXMLStringOutputAsyncAwait() async {
+		do {
+			for testBook in try getTestBooks() {
+				let epubCheck = EpubCheck(testBook)
+				let output = try await epubCheck.validate(output: .xml)
+				XCTAssertTrue(!output.isEmpty)
+
+				let xmlData = Data(output.utf8)
+				let xmlParser = XMLParser(data: xmlData)
+
+				let parsedSuccessfully = xmlParser.parse()
+				if parsedSuccessfully, let error = xmlParser.parserError {
+					XCTFail("\(error)")
+				}
+			}
+		} catch {
+			XCTFail("\(error)")
+		}
+	}
+	
+	func testValidationWithJSONStringOutputClosure() {
+		do {
+			for testBook in try getTestBooks() {
+				let epubCheck = EpubCheck(testBook)
+				epubCheck.validate(output: .json) { result in
+					switch result {
+					case .success(let output):
+						XCTAssertTrue(!output.isEmpty)
+					case .failure(let error):
+						XCTFail("\(error)")
+					}
+				}
+			}
+		} catch {
+			XCTFail("\(error)")
+		}
+	}
+	
+	func testValidationWithXMLStringOutputClosure() {
+		do {
+			for testBook in try getTestBooks() {
+				let epubCheck = EpubCheck(testBook)
+				epubCheck.validate(output: .xml) { result in
+					switch result {
+					case .success(let output):
+						XCTAssertTrue(!output.isEmpty)
+
+						let xmlData = Data(output.utf8)
+						let xmlParser = XMLParser(data: xmlData)
+
+						let parsedSuccessfully = xmlParser.parse()
+						if parsedSuccessfully, let error = xmlParser.parserError {
+							XCTFail("\(error)")
+						}
+					case .failure(let error):
+						XCTFail("\(error)")
+					}
+				}
+			}
+		} catch {
+			XCTFail("\(error)")
+		}
 	}
 
 	private func getTestBooks() throws -> [URL] {
